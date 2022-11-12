a8b9b61ae2844c8af4c37e0128ed5daec5cce3a0
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2022-11-12 10:18:02 +0100

    Added model for submission

---
+++ Homework1/submission/model.py
@@ -0,0 +1,15 @@
+import os
+import tensorflow.keras as tfk
+
+class model:
+    def __init__(self, path):
+        self.model = tfk.load_model(os.path.join(path, 'ANN_Homework1_Model'))
+
+    def predict(X, Y):
+
+        # Preprocessing
+
+        out = self.model.predict(X)
+        out = tf.argmax(out, axis=-1)
+
+        return out
