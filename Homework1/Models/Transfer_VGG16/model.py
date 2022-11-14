4e636436fd0700163042f9da4bcab92ad9b66ff2
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2022-11-14 14:12:52 +0100

    Added VGG16 model transfer learning draft

---
+++ Homework1/Models/Transfer_VGG16/model.py
@@ -0,0 +1,17 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+class model:
+    def __init__(self, path):
+        self.model = tf.keras.models.load_model(os.path.join(path, 'ANN_Homework1_Model'))
+
+    def predict(self, X):
+
+        # Preprocessing
+        X_scaled = X/255
+
+        out = self.model.predict(X_scaled)
+        out = tf.argmax(out, axis=-1)
+
+        return out
