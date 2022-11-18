d099121a0c0e3411c3fffb163c8e664476a3b6d5
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2022-11-18 01:33:43 +0100

    Added new VGG model

---
+++ Homework1/Models/VGG_GAP_ft/model.py
@@ -0,0 +1,16 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+class model:
+    def __init__(self, path):
+        self.model = tf.keras.models.load_model(os.path.join(path, 'ANN_Homework1_Model'))
+
+    def predict(self, X):
+
+        # Preprocessing 
+
+        out = self.model.predict(X)
+        out = tf.argmax(out, axis=-1)
+
+        return out
